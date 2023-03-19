ae1004dd62c2782cdced75e356b3b4883e30dbf7
Russ <yunsubin481@gmail.com>
2023-03-19 15:49:43 +0900

    memo

---
+++ src/Page/Memoization/Memo.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import Comments from './Comments';
+
+const commentList = [
+    {title:"comment1", content:"message1", like: 1},
+    {title:"comment2", content:"message2", like: 1},
+    {title:"comment3", content:"message3", like: 1}
+]
+export default function Memo() {
+    const [comments, setComments] = useState(commentList);
+    useEffect(() => {
+        const interval = setInterval(() =>{
+            setComments((prevComments)=>(
+                [...prevComments, {title:`comment${prevComments.length +1}`, content:`message${prevComments.length +1}`, like: `${Math.floor(Math.random() * 100) + 1}`}]
+            ))
+        },1000)
+        return () => clearInterval(interval)
+
+    },[])
+  return (
+    <Comments commentList={comments} />
+  )
+}
